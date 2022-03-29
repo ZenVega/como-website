a2932657bfcd8a742685d98bae3759bfb49d396f
Jan Weil <jan.weil@decarbon1ze.com>
2022-03-29 09:58:27 +0200

    Remove debug print

--- assets/js/como.js
+++ assets/js/como.js
@@ -1,7 +1,6 @@
 const navLinks = document.querySelectorAll('.nav-link')
 const offcanvas = new bootstrap.Offcanvas(document.getElementById('navbarOffcanvasLg'))
 navLinks.forEach(function (navLink) {
-    console.log(navLink)
     navLink.onclick = function () {
         offcanvas.hide()
     }
