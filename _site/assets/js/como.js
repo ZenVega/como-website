f1d2159056251b737b3da87f18eff79aedaa368a
Marco Kade <marco.kade@ts.berlin>
2022-04-07 11:56:58 +0200

    Added more links to local TTN community

--- _site/assets/js/como.js
+++
@@ -1,7 +0,0 @@
-const navLinks = document.querySelectorAll('.nav-link')
-const offcanvas = new bootstrap.Offcanvas(document.getElementById('navbarOffcanvasLg'))
-navLinks.forEach(function (navLink) {
-    navLink.onclick = function () {
-        offcanvas.hide()
-    }
-})
